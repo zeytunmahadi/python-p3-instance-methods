0a8e575bf7d17331bb2ee98b3bbeb72faf91c96a
zeitun <zeytunmahadi718@gmail.com>
2024-09-02 14:39:29 +0300

    Building instance methods for an object

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
+   
 
-    #Instance method definition
-    pass
+    def sit (self):
+        print("The dog is sitting.")
+    def bark(self):
+        print("Woof!")
+         
+       
+
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+    def talk(self):
+        print("Hello World!")
 
-    #Instance method definition
-    pass
+
+    def walk(self):
+        print("The person is walking.")
